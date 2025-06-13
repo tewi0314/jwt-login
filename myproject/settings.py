a3bde92d33c12b844074784c895743dd7f6db11d
tewi0314 <tewi0314@gmail.com>
2025-06-13 16:59:24 +0900

    Allow all hosts for EC2 access

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-6=7n%^e$d&u$n5n&eozb1hj%4f&340av49+l%#tr3c67h*mqq7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
